ddd2d64a078d5909cd8d94469ebd590b51634b17
Sergio Majé <smajefranco@gmail.com>
2024-03-09 12:00:00 -0500

    ➕ Add sitemap dependency for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 /* import svelte from '@astrojs/svelte'; */
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [tailwind(), mdx(), react(), vue(), /* svelte() */]
+  integrations: [tailwind(), mdx(), react(), vue() /* svelte() */, sitemap()]
 });
